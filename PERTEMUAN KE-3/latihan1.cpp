ca5acc886b76983e6aa9f686e810a91293f643b3
ISWANADAM <iswanadam071@gmail.com>
2025-02-19 16:12:08 +0800

    Add files via upload

---
+++ PERTEMUAN KE-3/latihan1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout <<"Hello word, Selamat Datang di Lab Programan"<<endl;
+    cout <<"Semester ini kita belajar C++ disini";
+    return 0;
+
+    }
