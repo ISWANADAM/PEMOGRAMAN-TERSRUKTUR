a82e093d0da80e0a496d2420a5f4f2b20d7d67d0
ISWANADAM <iswanadam071@gmail.com>
2025-02-19 15:53:35 +0800

    Add files via upload

---
+++ PERTEMUAN KE-3/latihan4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int bilangan;
+
+cout << "Masukan suatu bilangan : ";
+cin >> bilangan;
+
+cout << endl;
+
+cout << "Bilangan yang ada masukan adalah : " << bilangan << endl;
+cout << "LATIHAN 4 : ";
+
+return 0;
+}
