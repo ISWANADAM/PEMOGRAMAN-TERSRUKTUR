48e4de8af595c90e61ff137397ac0c5d2a01b8f2
ISWANADAM <iswanadam071@gmail.com>
2025-02-26 14:24:33 +0800

    Add files via upload

---
+++ PERTEMUAN KE-6/Latihan1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int tinggi, berat, selisih;
+
+    // Input tinggi badan dengan validasi tanpa while
+    cout << "Masukkan tinggi badan (cm): ";
+    cin >> tinggi;
+    if (tinggi <= 0) {
+        cout << "Tinggi badan harus lebih dari 0. Program dihentikan.\n";
+        return 1;
+    }
+
+    // Input berat badan dengan validasi tanpa while
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    if (berat <= 0) {
+        cout << "Berat badan harus lebih dari 0. Program dihentikan.\n";
+        return 1;
+    }
+
+    // Menghitung selisih tinggi badan dan berat badan
+    selisih = tinggi - berat;
+
+    // Menentukan kategori berat badan
+    if (selisih > 120) {
+        cout << "Kategori berat badan: Kegemukan" << endl;
+    } else if (selisih >= 111 && selisih <= 120) {
+        cout << "Kategori berat badan: Ideal" << endl;
+    } else if (selisih >= 100 && selisih <= 110) {
+        cout << "Kategori berat badan: Kurus" << endl;
+    } else {
+        cout << "Kategori berat badan: Sangat Kurus" << endl;
+    }
+
+    return 0;
+}
