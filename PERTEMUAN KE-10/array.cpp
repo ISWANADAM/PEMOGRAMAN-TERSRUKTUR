11f9c3db8288eb43065f9ecb5b489bab44223efc
ISWANADAM <iswanadam071@gmail.com>
2025-05-07 14:54:32 +0800

    Add files via upload

---
+++ PERTEMUAN KE-10/array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int nilai[5] = {85, 90, 78, 92, 88};
+    int max = nilai[0];
+    float total = 0;
+
+    // Hitung total dan cari nilai tertinggi
+    for (int i = 0; i < 5; i++) {
+        total += nilai[i];
+        if (nilai[i] > max) {
+            max = nilai[i];
+        }
+    }
+    cout << "Rata-rata: " << total / 5 << endl;
+    cout << "Nilai tertinggi: " << max << endl;
+    return 0;
+}
